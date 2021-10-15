5e86e4179c6a68bb11ebcebcefb61c3dd4c3e812
Brigham Andersen <brighamband@gmail.com>
2021-10-15 15:21:56 -0600

    last one done

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -66,8 +66,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        return n + RandomStringUtils.randomAlphabetic(10);
     }
 
 
